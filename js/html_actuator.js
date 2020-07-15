ef33f18ced368892e8fbdab056ee7151df48bbe4
Tarun Singh <35271830+tarunsingh5@users.noreply.github.com>
2020-07-15 10:06:13 -0700

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "ਤੁਸੀਂ ਜਿੱਤ ਗਏ!" : "ਖੇਲ ਖਤਮ!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
